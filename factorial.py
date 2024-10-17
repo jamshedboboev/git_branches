4b88fa559c0b849f296af3fd687b2cc6baa6de31
Boboev Jamshed <jamikaki002@gmail.com>
2024-10-17 13:08:07 +0500

    fix problem with methods

--- factorial.py
+++ factorial.py
@@ -4,9 +4,13 @@ def factorial(n, method="iterative"):
             return 1
         else:
             return n * factorial(n - 1, method="recursive")
+    elif method == "iterative":
+        result = 1
+        for i in range(1, n + 1):
+            result *= i
+        return result
     else:
-        # Placeholder for iterative method
-        pass
+        print('Wrong method')
 
 if __name__ == "__main__":
     number = 5
